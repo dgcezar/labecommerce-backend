f2c5d42521066834f73e8133a313d99bb6f94816
dgcezar <diego.cezar13@gmail.com>
2023-01-04 09:37:26 -0300

    exercicios ts 2 feito

--- src/database.ts
+++ src/database.ts
@@ -1,3 +1,4 @@
+import { CATEGORY } from "./enum";
 import { TProduct, TPurchase, TUser } from "./types";
 
 export const users: TUser[] = [
@@ -13,20 +14,64 @@ export const users: TUser[] = [
   },
 ];
 
+export function createUser(id: string, email: string, password: string): void {
+  const newUser: TUser = { id: id, email: email, password: password };
+  users.push(newUser);
+  console.log("Usuário criado com sucesso");
+}
+
+export function getAllUsers(): TUser[] {
+  return users;
+}
+
 export const products: TProduct[] = [
   {
     id: "p01",
     name: "Produto1",
     price: 40,
-    category: "roupas-camiseta",
+    category: CATEGORY.CLOTHES_AND_SHOES,
   },
   {
     id: "p02",
     name: "Produto2",
     price: 20,
-    category: "roupas-chinelo",
+    category: CATEGORY.ACCESSORIES,
   },
 ];
+
+export function createProduct(
+  id: string,
+  name: string,
+  price: number,
+  category: CATEGORY
+): void {
+  const newProduct: TProduct = {
+    id: id,
+    name: name,
+    price: price,
+    category: category,
+  };
+  products.push(newProduct);
+  console.log("produto criado com sucesso");
+}
+
+export function getAllProducts(): TProduct[] {
+  return products;
+}
+
+export function getProductById(idToSearch: string): TProduct[] | undefined {
+  return products.filter((product) => {
+    if (product.id === idToSearch) {
+      return product;
+    }
+  });
+}
+
+export function queryProductsByName(q: string) {
+  return products.filter((product) => {
+    return product.name.toLowerCase().includes(q.toLowerCase());
+  });
+}
 
 export const purchases: TPurchase[] = [
   {
@@ -42,3 +87,27 @@ export const purchases: TPurchase[] = [
     totalPrice: products[1].price * 6,
   },
 ];
+
+export function createPurchase(
+  userId: string,
+  productId: string,
+  quantity: number,
+  totalPrice: number
+): void {
+  const newPurchase: TPurchase = {
+    userId: userId,
+    productId: productId,
+    quantity: quantity,
+    totalPrice: totalPrice,
+  };
+  purchases.push(newPurchase);
+  console.log("compra realizada com sucesso");
+}
+
+export function getAllPurchasesFromUserid(userIdToSearch: string): TPurchase[] | undefined {
+    return purchases.filter((purchase) => {
+    if (purchase.userId === userIdToSearch) {
+      return purchase;
+    }
+  });
+}
