128a9a52f2e42dafacc1e3093df478a6963d97ec
dgcezar <diego.cezar13@gmail.com>
2023-01-09 08:57:55 -0300

    Express e cors instalados + teste da API

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
-import { createProduct, createPurchase, createUser, getAllProducts, getAllPurchasesFromUserid, getAllUsers, getProductById, products, purchases, users } from "./database";
-import { CATEGORY } from "./enum";
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import { TUser, TProduct, TPurchase } from './types'
 
-createUser("0922", "asd@gmail.com", "92041")
-console.log(getAllUsers())
-console.log(createProduct("p033", "Pulseira", 12, CATEGORY.ACCESSORIES))
-console.log(getAllProducts())
-console.log(getProductById("p01"))
-console.log(createPurchase("01", "p02", 2, 5))
-console.log(getAllPurchasesFromUserid("01"))
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+})
+
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong!')
+})
+
+// import { createProduct, createPurchase, createUser, getAllProducts, getAllPurchasesFromUserid, getAllUsers, getProductById, products, purchases, users } from "./database";
+// import { CATEGORY } from "./enum";
+
+// createUser("0922", "asd@gmail.com", "92041")
+// console.log(getAllUsers())
+// console.log(createProduct("p033", "Pulseira", 12, CATEGORY.ACCESSORIES))
+// console.log(getAllProducts())
+// console.log(getProductById("p01"))
+// console.log(createPurchase("01", "p02", 2, 5))
+// console.log(getAllPurchasesFromUserid("01"))
