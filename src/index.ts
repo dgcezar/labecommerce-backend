b133edee1135b1bff61b353548bf3610f3985f77
dgcezar <diego.cezar13@gmail.com>
2023-01-09 08:39:46 -0300

    endpoints do exercício 3 criados

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { TUser, TProduct, TPurchase } from './types'
-import { products, users } from './database'
+import { products, purchases, users } from './database'
 
 const app = express()
 
@@ -32,6 +32,50 @@ app.get('/products/search', (req: Request, res: Response) => {
     })
 
     res.status(200).send(result)
+})
+
+app.post('/users', (req: Request, res: Response) => {
+    const {id, email, password} = req.body as TUser
+
+    const newUser = {
+        id,
+        email,
+        password
+    }
+
+    users.push(newUser)
+
+    res.status(201).send("Usuário registrado com sucesso!")
+})
+
+app.post('/products', (req: Request, res: Response) => {
+    const {id, name, price, category} = req.body as TProduct
+
+    const newProduct = {
+        id,
+        name,
+        price,
+        category
+    }
+
+    products.push(newProduct)
+
+    res.status(201).send("Produto registrado com sucesso!")
+})
+
+app.post('/purchases', (req: Request, res: Response) => {
+    const {userId, productId, quantity, totalPrice} = req.body as TPurchase
+
+    const newPurchase = {
+        userId,
+        productId,
+        quantity,
+        totalPrice
+    }
+
+    purchases.push(newPurchase)
+
+    res.status(201).send("Compra realizada com sucesso")
 })
 
 // import { createProduct, createPurchase, createUser, getAllProducts, getAllPurchasesFromUserid, getAllUsers, getProductById, products, purchases, users } from "./database";
